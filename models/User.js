7f44d227865a9e60e4b364ac19bf1db30f605864
Samir007-rb <ranasamir.bhat@gmail.com>
2025-03-29 20:18:22 -0400

    update admin panel

--- models/User.js
+++ models/User.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: false },
   email: { type: String, required: true, unique: true },
-  role: { type: String, enum: ['admin', 'user'], default: 'user' }
+  role: { type: String, enum: ['admin', 'user'], default: 'user' },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('User', UserSchema);
